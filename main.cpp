e7b1b0afadaa083e04617c913adb9e1b3d1a5756
yzkadbq <122332275+yzkadbq@users.noreply.github.com>
2023-01-28 19:08:21 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,15 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+struct strt{
+  int val_cnt,val_str,val_int,val_bool;
+  string [100][100];
+  int [100][100];
+  bool [100][100];
+} stct[100];
+int strt_cnt=0;
+void init(){
+  
+}
+int main(){
+  return 0;
+}
